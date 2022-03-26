2b54b6baf6f90bf4fc944ed346cb895ac190cfc8
Alekhya <alekhya.duba@tamu.edu>
2022-03-26 18:45:27 -0500

    Add constraints on the Parking spot models

--- adminhome/models.py
+++ adminhome/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class ParkingCategory(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     size = models.DecimalField(decimal_places=2)
     rent_daily_rate = models.DecimalField(decimal_places=2)
     rent_weekly_rate = models.DecimalField(decimal_places=2)
@@ -17,7 +17,7 @@ class ParkingCategory(models.Model):
 
 
 class ParkingSpot(models.Model):
-    name = models.CharField(max_lenth=200)
+    name = models.CharField(max_lenth=200, unique=True)
     is_active = models.BooleanField()
     parking_category_id = models.ForeignKey(ParkingCategory, on_delete=models.CASCADE, related_name="parking_spot")
 
