99b1cb1a6bd7ac83923ddc0ddcc09101515f91ca
Alekhya <alekhya.duba@tamu.edu>
2022-03-26 18:40:49 -0500

    Add models for the parking category and parking spots

--- adminhome/models.py
+++ adminhome/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ParkingCategory(models.Model):
+    name = models.CharField(max_length=200)
+    size = models.DecimalField(decimal_places=2)
+    rent_daily_rate = models.DecimalField(decimal_places=2)
+    rent_weekly_rate = models.DecimalField(decimal_places=2)
+    rent_monthly_rate = models.DecimalField(decimal_places=2)
+    utility_rate = models.DecimalField(decimal_places=2)
+    is_active = models.BooleanField()
+    min_time_window = models.IntegerField()
+    cancellation_charges = models.DecimalField(decimal_places=2)
+    cancellation_policy = models.IntegerField()
+
+
+class ParkingSpot(models.Model):
+    name = models.CharField(max_lenth=200)
+    is_active = models.BooleanField()
+    parking_category_id = models.ForeignKey(ParkingCategory, on_delete=models.CASCADE, related_name="parking_spot")
+
