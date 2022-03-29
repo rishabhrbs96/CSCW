05de87ef24c35fce560bd53be837fd9b43905938
Alekhya <alekhya.duba@tamu.edu>
2022-03-29 00:58:17 -0500

    pulling changes from iter-2

--- adminhome/forms.py
+++ adminhome/forms.py
@@ -33,3 +33,21 @@ class HomeForm(forms.Form):
             self.fields['carousel_header_%s' % i] = forms.CharField(label=('Carousel %s Header: ' % (i+1)), initial=c["header"], widget= forms.TextInput(attrs={'class':'form-control'}))
             self.fields['carousel_body_%s' % i] = forms.CharField(label=('Carousel %s Contents: ' % (i+1)), initial=c["body"], widget= forms.Textarea(attrs={'class':'form-control'}))
             self.fields['carousel_image_%s' % i] = forms.ImageField(label=('Carousel %s Image: ' % (i+1)), required=False)
+
+class CreateParkingSpotCategoryForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(CreateParkingSpotCategoryForm, self).__init__(*args, **kwargs)
+
+
+    class Meta:
+        model = ParkingCategory
+        fields = "__all__"
+
+class CreateParkingSpotForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(CreateParkingSpotForm, self).__init__(*args, **kwargs)
+
+
+    class Meta:
+        model = ParkingSpot
+        fields = "__all__"
