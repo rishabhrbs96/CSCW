ed19651328642b9630e05fda22200eb3e63851e5
Mudit <mudit.maheshwari1996@gmail.com>
2022-04-18 18:08:43 -0500

    Add form to verify vehicle

--- adminhome/forms.py
+++ adminhome/forms.py
@@ -101,3 +101,13 @@ class VehicleForm(forms.ModelForm):
     class Meta:
         model = Vehicle
         fields = ['name', 'model', 'make', 'build', 'color', 'insurance_doc']
+
+
+class VerifyVehicleForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(VerifyVehicleForm, self).__init__(*args, **kwargs)
+        self.fields['insurance_expiry_date'] = forms.DateTimeField(help_text="Format yyyy-mm-dd")
+
+    class Meta:
+        model = Vehicle
+        fields = ['insurance_expiry_date']
